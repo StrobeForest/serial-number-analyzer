302c83c37cfebebf3acfca88d4e568181130e83a
Nick <raviolisauce@gmail.com>
2019-03-02 11:33:30 -0500

    Created intro/tutorial on how to find and submit a bills serial number.

---
+++ s-n-a.py
@@ -0,0 +1,9 @@
+correct_input = False
+print("Hello User!")
+print("I will take in your bills serial number, and tell you all about it.")
+print("I am only able to analyze the serial numbers on $1 bills due to the higher denominations being of a different format.")
+print("The serial number can be found twice on the front of the dollar bill written in green.")
+print("It appears once on either side of Washington, and is 8 digits with a letter at the beginning and the end.")
+print("Some examples of $1 serial numbers are: B03542754F, L01986303*, F03779718I, and C81230251D")
+print("Please submit your bills serial number below in the format found in the examples above.")
+serial = input("--->")
